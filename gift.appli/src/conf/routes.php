222dfce40d9638b0e2c43defa3308ec5d9a4fe44
Ozen Burak <burak.ozen6720@gmail.com>
2025-06-04 15:00:50 +0200

    modif home + ajout liste prestations action

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -1,8 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use giftbox\webui\actions\GetCategorieByIdAction;
+use giftbox\webui\actions\GetAllPrestationsAction;
 use giftbox\webui\actions\GetCategoriesAction;
+use giftbox\webui\actions\GetCategorieByIdAction;
 use giftbox\webui\actions\GetDetailCoffretAction;
 use giftbox\webui\actions\GetListeCoffretsAction;
 use giftbox\webui\actions\GetPrestationAction;
@@ -21,6 +22,7 @@ return function (App $app) : App {
     $app->get('/prestation', GetPrestationAction::class)->setName('prestation');
     $app->get('/coffrets', GetListeCoffretsAction::class)->setName('coffrets');
     $app->get('/coffret/{id}', GetDetailCoffretAction::class)->setName('coffret');
+    $app->get('/prestations', GetAllPrestationsAction::class)->setName('prestations');
     $app->map(['GET', 'POST'], '/box', CreateBoxAction::class);
     $app->map(['GET', 'POST'], '/box/{id}/add', AddPrestationToBoxAction::class);
     $app->get('/box/{id}', GetBoxAction::class);
