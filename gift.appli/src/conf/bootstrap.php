ffeafb827d010c59975594b04d3a5705f5667a64
Ozen Burak <burak.ozen6720@gmail.com>
2025-06-08 22:22:06 +0200

    modif bootstrap

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use DI\Container;
 use giftbox\application_core\application\providers\AuthProviderInterface;
 use giftbox\application_core\application\providers\SessionAuthProvider;
 use giftbox\application_core\application\useCases\AuthnService;
@@ -10,8 +11,25 @@ use giftbox\infrastructure\EloquentUserRepository;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use Psr\Container\ContainerInterface;
 use giftbox\application_core\application\useCases\BoxService;
 
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set(UserRepositoryInterface::class, function() {
+    return new EloquentUserRepository();
+});
+
+$container->set(AuthnServiceInterface::class, function($c) {
+    return new AuthnService($c->get(UserRepositoryInterface::class));
+});
+
+$container->set(AuthProviderInterface::class, function($c) {
+    return new SessionAuthProvider(
+        $c->get(AuthnServiceInterface::class),
+        $c->get(UserRepositoryInterface::class));
+});
 
 $app = AppFactory::create();
 
@@ -29,20 +47,6 @@ $twig = Twig::create(__DIR__ . '/../webui/views', ['cache' => false]);
 $app->add(TwigMiddleware::create($app, $twig));
 $twig->getEnvironment()->addGlobal('routeParser', $app->getRouteCollector()->getRouteParser());
 
-
-$container->set(UserRepositoryInterface::class, function() {
-    return new EloquentUserRepository();
-});
-
-$container->set(AuthnServiceInterface::class, function($c) {
-    return new AuthnService($c->get(UserRepositoryInterface::class));
-});
-
-$container->set(AuthProviderInterface::class, function($c) {
-    return new SessionAuthProvider(
-        $c->get(AuthnServiceInterface::class),
-        $c->get(UserRepositoryInterface::class));
-});
 
 (require_once __DIR__ . '/routes.php')($app);
 (require_once __DIR__ . '/../api/routes.php')($app);
